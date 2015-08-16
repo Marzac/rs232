163b082404ab71abde6105524a792e30e33aee6d
Fred <frederic.meslin@waldorfmusic.de>
2015-08-16 14:40:53 +0200

    Doxygen documentation

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # rs232
 C / C++ RS232 cross-platform serial library
-Support Windows (XP, Win7) / Linux and Mac OS X systems
+
+Supported platforms:  
+- Windows (XP / Win7, possibly 8 and 10)  
+- Linux  
+- MacOS X  
 
 Copyright (c) 2013-2015 Frédéric Meslin  
 Email: fredericmeslin@hotmail.com  
--- rs232.h
+++ rs232.h
@@ -37,20 +37,111 @@
     #include <stdlib.h>
 
 /*****************************************************************************/
+    /* Doxywizard specific */
+    /**
+    * \mainpage RS232
+    * \section intro_sec C / C++ RS232 cross-platform serial library
+    * <b>Version 0.2 - 16/08/2015</b>
+	*
+    * Supported platforms:  
+    * - Windows (XP / Win7, possibly 8 and 10)  
+    * - Linux  
+    * - MacOS X  
+    *
+    * Copyright (c) 2013-2015 Fr&eacute;d&eacute;ric Meslin, Florent Touchard <br>
+    * Email: fredericmeslin@hotmail.com <br>
+    * Website: www.fredslab.net <br>
+    * Twitter: \@marzacdev <br>
+    */
+    
+/*****************************************************************************/
+    /**
+     * \fn int comEnumerate()
+     * \brief Enumerate available serial ports (Serial, USB serial, Bluetooth serial)
+     * \return number of enumerated ports
+     */
     int comEnumerate();
+    
+    /**
+     * \fn int comGetNoPorts()
+     * \brief Return the number of enumerated ports
+     * \return number of enumerated ports
+     */
     int comGetNoPorts();
+
+    /**
+     * \fn int comTerminate()
+     * \brief Release ports and memory resources used by the library
+     */    
     void comTerminate();
 
+    /**
+     * \fn const char * comGetPortName(int index)
+     * \brief Get port user-friendly name
+     * \param[in] index port index
+     * \return null terminated port name
+     */    
     const char * comGetPortName(int index);
-    int comFindPort(const char * name);
+
+    /**
+     * \fn const char * comGetInternalName(int index)
+     * \brief Get port operating-system name
+     * \param[in] index port index
+     * \return null terminated port name
+     */        
     const char * comGetInternalName(int index);
+    
+    /**
+     * \fn int comFindPort(const char * name)
+     * \brief Try to find a port given its user-friendly name
+     * \param[in] name port name (case sensitive)
+     * \return index of found port or -1 if not enumerated
+     */        
+    int comFindPort(const char * name);
 
 /*****************************************************************************/
-    int  comOpen(int index, int baudrate);
+    /**
+     * \fn int comOpen(int index, int baudrate)
+     * \brief Try to open a port at a specific baudrate
+     * \brief (No parity, single stop bit, no hardware flow control)
+     * \param[in] index port index
+     * \param[in] baudrate port baudrate
+     * \return 1 if opened, 0 if not available
+     */        
+    int comOpen(int index, int baudrate);
+    
+    /**
+     * \fn void comClose(int index)
+     * \brief Close an opened port
+     * \param[in] index port index
+     */            
     void comClose(int index);
+    
+    /**
+     * \fn void comCloseAll()
+     * \brief Close all opened ports
+     */            
     void comCloseAll();
 
+/*****************************************************************************/
+    /**
+     * \fn int comWrite(int index, const char * buffer, size_t len)
+     * \brief Write data to the port (non-blocking)
+     * \param[in] index port index
+     * \param[in] buffer pointer to transmit buffer
+     * \param[in] len length of transmit buffer in bytes
+     * \return number of bytes transferred
+     */            
     int comWrite(int index, const char * buffer, size_t len);
+    
+    /**
+     * \fn int comRead(int index, const char * buffer, size_t len)
+     * \brief Read data from the port (non-blocking)
+     * \param[in] index port index
+     * \param[in] buffer pointer to receive buffer
+     * \param[in] len length of receive buffer in bytes
+     * \return number of bytes transferred
+     */                
     int comRead(int index, char * buffer, size_t len);
 
 #endif // RS232_H
